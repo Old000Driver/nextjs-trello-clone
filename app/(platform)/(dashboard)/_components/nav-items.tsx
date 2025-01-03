cbb7914f60418b7d843e18db74914027a133de68
GJA <792002710@qq.com>
2025-01-03 17:24:56 +0800

    feat(organization): 为组织页面添加侧边栏并优化页面布局
    - 在组织布局中添加了侧边栏组件
    - 更新了拼写检查词汇表，增加了新单词 usehooks
    - 在项目中集成了 usehooks-ts 库
    - 配置了 Tailwind CSS 动画，支持手风琴组件的折叠动画
    - 更新了 package.json 和 package-lock.json，添加了新依赖项

---
+++ app/(platform)/(dashboard)/_components/nav-items.tsx
@@ -0,0 +1,109 @@
+"use client";
+
+import {
+  AccordionItem,
+  AccordionTrigger,
+  AccordionContent,
+} from "@/components/ui/accordion";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import { useRouter, usePathname } from "next/navigation";
+import { Button } from "@/components/ui/button";
+
+import { Activity, CreditCard, Layout, Settings } from "lucide-react";
+
+export type Organization = {
+  id: string;
+  slug: string;
+  imageUrl: string;
+  name: string;
+};
+
+interface NavItemProps {
+  organization: Organization;
+  isExpanded: boolean;
+  isActive: boolean;
+  onExpand: (id: string) => void;
+}
+const NavItem = ({
+  isExpanded,
+  isActive,
+  onExpand,
+  organization,
+}: NavItemProps) => {
+  const router = useRouter();
+  const pathname = usePathname();
+  const routes = [
+    {
+      icon: <Layout className="w-4 h-4 mr-2" />,
+      label: "Boards",
+      href: `/organization/${organization.id}`,
+    },
+    {
+      icon: <Activity className="w-4 h-4 mr-2" />,
+      label: "Activity",
+      href: `/organization/${organization.id}/activity`,
+    },
+    {
+      icon: <Settings className="w-4 h-4 mr-2" />,
+      label: "Settings",
+      href: `/organization/${organization.id}/settings`,
+    },
+    {
+      icon: <CreditCard className="w-4 h-4 mr-2" />,
+      label: "Billing",
+      href: `/organization/${organization.id}/billing`,
+    },
+  ];
+
+  const onClick = (href: string) => {
+    // const isClicked = pathname === href;
+    // if (isClicked) {
+    //   return router.push(href);
+    // }
+    router.push(href);
+  };
+
+  return (
+    <AccordionItem value={organization.id} className="border-none">
+      <AccordionTrigger
+        onClick={() => onExpand(organization.id)}
+        className={cn(
+          "flex items-center gap-x-2 p-1.5 text-neutral-700 rounded-md hover:bg-neutral-500/10 transition text-start no-underline hover:no-underline",
+          isActive && !isExpanded && "bg-sky-500/10 text-sky-700"
+        )}
+      >
+        <div className="flex items-center gap-x-2">
+          <div className="w-7 h-7 relative">
+            <Image
+              fill
+              src={organization.imageUrl}
+              alt="Organization"
+              className="rounded-sm object-cover"
+            />
+          </div>
+          <span className="font-medium text-sm">{organization.name}</span>
+        </div>
+      </AccordionTrigger>
+      <AccordionContent className="pt-1 text-neutral-700">
+        {routes.map((route) => (
+          <Button
+            key={route.href}
+            size={"sm"}
+            onClick={() => onClick(route.href)}
+            className={cn(
+              "w-full font-normal justify-start pl-10 mb-1",
+              pathname === route.href && "bg-sky-500/10 text-sky-700"
+            )}
+            variant={"ghost"}
+          >
+            {route.icon}
+            {route.label}
+          </Button>
+        ))}
+      </AccordionContent>
+    </AccordionItem>
+  );
+};
+
+export default NavItem;
---
+++ app/(platform)/(dashboard)/_components/sideBar.tsx
@@ -0,0 +1,91 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Skeleton } from "@/components/ui/skeleton";
+import { Plus } from "lucide-react";
+import { useOrganization, useOrganizationList } from "@clerk/nextjs";
+import Link from "next/link";
+import { useLocalStorage } from "usehooks-ts";
+import { Accordion } from "@/components/ui/accordion";
+import NavItem, { Organization } from "./nav-items";
+
+interface SidebarProps {
+  storageKey?: string;
+}
+
+const Sidebar = ({ storageKey = "t-sidebar-state" }: SidebarProps) => {
+  const [expanded, setExpanded] = useLocalStorage<Record<string, any>>(
+    storageKey,
+    {}
+  );
+
+  // 重命名 organization 为 activeOrganization
+  const { organization: activeOrganization, isLoaded: isLoadedOrg } =
+    useOrganization();
+
+  const { userMemberships, isLoaded: isLoadedOrgList } = useOrganizationList({
+    userMemberships: { infinite: true },
+  });
+
+  // console.log("⚠️ expanded", expanded);
+  console.log("⚠️ userMemberships", userMemberships);
+  const defaultAccordionValue: string[] = Object.keys(expanded).reduce(
+    (acc: string[], key: string) => {
+      if (expanded[key]) {
+        acc.push(key);
+      }
+      return acc;
+    },
+    []
+  );
+  const onExpand = (id: string) => {
+    setExpanded((curr) => ({
+      ...curr,
+      [id]: !expanded[id],
+    }));
+  };
+
+  if (!isLoadedOrg || !isLoadedOrgList || userMemberships.isLoading) {
+    return (
+      <>
+        <Skeleton />
+      </>
+    );
+  }
+  return (
+    <>
+      <div className="font-medium text-xs flex items-center mb-1">
+        <span className="pl-4">WorkSpace</span>
+        <Button
+          asChild
+          type="button"
+          size="icon"
+          variant="ghost"
+          className="ml-auto"
+        >
+          <Link href="/select-org">
+            <Plus className="h-4 w-4" />
+          </Link>
+        </Button>
+      </div>
+      <Accordion
+        type="multiple"
+        defaultValue={defaultAccordionValue}
+        className="space-y-2"
+      >
+        {userMemberships.data.map(({ organization }) => (
+          <NavItem
+            key={organization.id}
+            isActive={activeOrganization?.id === organization.id}
+            isExpanded={expanded[organization.id]}
+            onExpand={onExpand}
+            organization={organization as Organization}
+          ></NavItem>
+          // <p key={organization.id}>{organization.id}</p>
+        ))}
+      </Accordion>
+    </>
+  );
+};
+
+export default Sidebar;
