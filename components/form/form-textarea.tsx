d13bfbf72d759abe724215b9f71f6683cac91a61
GJA <792002710@qq.com>
2025-01-17 10:53:26 +0800

    feat(platform): 优化列表组件并添加卡片表单功能
    - 在 ListHeader 组件中添加 onAddCard 属性，用于触发添加卡片操作
    - 实现 ListItem 组件的编辑功能和卡片列表展示
    - 新增 CardForm 组件用于添加新卡片
    - 优化列表项的样式和布局

---
+++ components/form/form-textarea.tsx
@@ -0,0 +1,80 @@
+"use client";
+
+import { forwardRef, KeyboardEventHandler } from "react";
+import { Label } from "@/components/ui/label";
+import { cn } from "@/lib/utils";
+
+interface FormTextareaProps {
+  id: string;
+  label?: string;
+  placeholder?: string;
+  required?: boolean;
+  disabled?: boolean;
+  errors?: Record<string, string[] | undefined>;
+  className?: string;
+  onBlur?: () => void;
+  onClick?: () => void;
+  onKeyDown?: KeyboardEventHandler<HTMLTextAreaElement> | undefined;
+  defaultValue?: string;
+}
+
+import React from "react";
+import { Textarea } from "../ui/textarea";
+import FormErrors from "./form-errors";
+import { useFormState, useFormStatus } from "react-dom";
+
+export const FormTextArea = forwardRef<HTMLTextAreaElement, FormTextareaProps>(
+  (
+    {
+      id,
+      label,
+      placeholder,
+      required,
+      disabled,
+      errors,
+      className,
+      onBlur,
+      onClick,
+      onKeyDown,
+      defaultValue,
+    }: FormTextareaProps,
+    ref
+  ) => {
+    const { pending } = useFormStatus();
+
+    return (
+      <div className="space-y-2 w-full">
+        <div className="space-y-1 w-full">
+          {label ? (
+            <Label
+              htmlFor={id}
+              className="text-xs font-semibold text-neutral-700"
+            >
+              {label}
+            </Label>
+          ) : null}
+          <Textarea
+            onKeyDown={onKeyDown}
+            onBlur={onBlur}
+            onClick={onClick}
+            ref={ref}
+            id={id}
+            name={id}
+            placeholder={placeholder}
+            required={required}
+            disabled={pending || disabled}
+            className={cn(
+              "resize-none focus-visible:ring-0 focus-visible:ring-offset-0 ring-0 focus:ring-0 outline-none shadow-sm",
+              className
+            )}
+            defaultValue={defaultValue}
+            aria-describedby={`${id}-error`}
+          />
+        </div>
+        <FormErrors id={id} errors={errors} />
+      </div>
+    );
+  }
+);
+
+FormTextArea.displayName = "FormTextArea";
