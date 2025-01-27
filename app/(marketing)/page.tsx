396858ba58412f62a65f33ada117812e2e8b15f7
GJA <792002710@qq.com>
2025-01-27 15:27:17 +0800

    refactor(marketing): 调整页面标题和文本样式以提升可读性

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -36,11 +36,11 @@ const MarketingPage = () => {
           <Medal className="h-6 w-6 mr-2" />
           No 1 任务管理
         </div>
-        <h1 className="text-3xl md:text-6xl text-center text-neutral-800 mb-6">
+        <h1 className="text-3xl md:text-4xl flex text-center text-neutral-800 mb-6">
           Taskify 帮助团队前进
         </h1>
-        <div className="text-3xl md:text-6xl bg-gradient-to-r from-fuchsia-600 to-pink-600 text-white px-4 p-2 rounded-md pb-4 w-fit">
-          向前工作
+        <div className="text-3xl md:text-4xl text-center bg-gradient-to-r from-fuchsia-600 to-pink-600 text-white px-4 rounded-md p-4 w-fit">
+          持续前进
         </div>
         <div
           className={cn(
